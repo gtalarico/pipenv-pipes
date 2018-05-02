77caffa7720d5a9d297376b235a9638767986bc8
gtalarico <gtalarico@gmail.com>
2018-05-02 01:46:16 -0400

    Fixed expand user error on unix

--- pipenv_pipes/environment.py
+++ pipenv_pipes/environment.py
@@ -8,7 +8,7 @@ import os
 default_home = os.path.join(
     os.environ.get('XDG_DATA_HOME', '~/.local/share'), 'virtualenvs')
 
-PIPENV_HOME = os.getenv('WORKON_HOME', default_home)
+PIPENV_HOME = os.path.expanduser(os.getenv('WORKON_HOME', default_home))
 PIPENV_ACTIVE = os.getenv('PIPENV_ACTIVE', 0) == '1'
 PROMPT = os.getenv('PROMPT', '')
 PIPENV_VENV_IN_PROJECT = os.getenv('PIPENV_VENV_IN_PROJECT')
