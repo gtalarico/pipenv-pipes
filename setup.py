4e2ebcc400ff7f6ff49fa665e4d0d0d619669ad0
gtalarico <gtalarico@gmail.com>
2018-05-02 13:49:14 -0400

    Bump version: 0.1.2 -> 0.2.0

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gtalarico/pipenv-pipes',
-    version='0.1.2',
+    version='0.2.0',
     zip_safe=False,
 )
