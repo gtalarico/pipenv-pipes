5e658e160cf3aca28a26782bd76def2ea43fd5bf
gtalarico <gtalarico@gmail.com>
2018-05-10 11:01:58 -0400

    Removed Pexpext depency, simplified Process calls

--- pipenv_pipes/pipenv.py
+++ pipenv_pipes/pipenv.py
@@ -1,19 +1,25 @@
 import os
 import sys
+from subprocess import Popen, PIPE
 
-import subprocess
-from pexpect.popen_spawn import PopenSpawn
+
+def PipedPopen(*args, **kwargs):
+    """ Helper Piped Process for drier code"""
+    timeout = kwargs.pop('timeout', None)
+    proc = Popen(*args, **kwargs,
+                 stdout=PIPE,
+                 stderr=PIPE,
+                 universal_newlines=True,
+                )
+    out, err = proc.communicate(timeout=timeout)
+    output = out.strip() or err.strip()
+    code = proc.returncode
+    return output.strip(), code
 
 
 def call_pipenv_venv(project_dir, timeout=10):
     """ Calls ``pipenv --venv`` from a given project directory """
-    proc = PopenSpawn(
-        'pipenv --venv',
-        cwd=project_dir,
-        timeout=timeout)
-
-    code = proc.wait()
-    output = proc.readline().decode().strip()
+    output, code = PipedPopen(['pipenv', '--venv'], cwd=project_dir)
     return output, code
 
 
@@ -23,10 +29,10 @@ def call_pipenv_shell(cwd, envname='pipenv-shell'):
     environ['PROMPT'] = '({}){}'.format(envname, os.getenv('PROMPT', ''))
 
     is_test = 'PYTEST_CURRENT_TEST' in os.environ
-    timeout = 5 if is_test else None
-    stdout = subprocess.PIPE if is_test else sys.stdout
+    timeout = 10 if is_test else None
+    stdout = PIPE if is_test else sys.stdout
 
-    proc = subprocess.Popen(
+    proc = Popen(
         ['pipenv', 'shell'],
         cwd=cwd,
         shell=False,
