16ff886d23dcbd9d451c4dd69a675087f4c90c81
gtalarico <gtalarico@gmail.com>
2018-05-13 11:42:49 -0400

    Bump version: 0.5.0 -> 0.5.0-post.1

--- pipenv_pipes/__init__.py
+++ pipenv_pipes/__init__.py
@@ -3,6 +3,6 @@
 """ Pipes: Pipenv Shell Switcher """
 
 __author__ = """Gui Talarico"""
-__version__ = '0.5.0'
+__version__ = '0.5.0-post.1'
 __email__ = 'gui.talarico+pip@gmail.com'
 __url__ = 'github.com/gtalarico/pipenv-pipes'
